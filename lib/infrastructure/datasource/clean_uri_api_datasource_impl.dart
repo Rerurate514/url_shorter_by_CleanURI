7b4cca78c988b479dc116d5a5ac3e7941c5d17ab
Rerurate514 <rerurateyuto@gmail.com>
2025-07-01 21:35:55 +0900

    GETではなくPOSTを使用するように変更

--- lib/infrastructure/datasource/clean_uri_api_datasource_impl.dart
+++ lib/infrastructure/datasource/clean_uri_api_datasource_impl.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:url_shorter_by_cleanuri/core/exception/network_exception.dart';
 import 'package:url_shorter_by_cleanuri/core/utils/dio.dart';
@@ -22,19 +22,52 @@ class CleanUriApiDatasourceImpl implements CleanUriApiDatasource {
 
   @override
   Future<ShortUrlResponse> getShortUrl({String url = ""}) async {
-    final res = await _dio.get(
-      'https://cleanuri.com/api/v1/shorten',
-      queryParameters: {
-        "url": url
-      }
-    );
+    try {
+      final res = await _dio.post(
+        'https://cleanuri.com/api/v1/shorten',
+        data: 'url=${Uri.encodeComponent(url)}',
+        options: Options(
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        ),
+      );
 
-    if(res.statusCode == 200){
-      final body = res.data;
-      final data = ShortUrlResponse.fromJson(body);
-      return data;
-    } else {
-      throw NetworkException(statusCode: res.statusCode ?? 400, message: res.extra["result_url"]);
+      if (res.statusCode == 200) {
+        final body = res.data;
+        
+        if (body['error'] != null) {
+          throw NetworkException(
+            statusCode: 400, 
+            message: body['error']
+          );
+        }
+        
+        final data = ShortUrlResponse.fromJson(body);
+        return data;
+      } else {
+        throw NetworkException(
+          statusCode: res.statusCode ?? 400, 
+          message: 'HTTP ${res.statusCode}: ${res.statusMessage}'
+        );
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        throw NetworkException(
+          statusCode: e.response!.statusCode ?? 400,
+          message: e.response!.data?['error'] ?? e.message ?? 'Network error'
+        );
+      } else {
+        throw NetworkException(
+          statusCode: 0,
+          message: e.message ?? 'Network error'
+        );
+      }
+    } catch (e) {
+      throw NetworkException(
+        statusCode: 0,
+        message: e.toString()
+      );
     }
   }
 }
